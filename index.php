6891811720366a31a61d3fdd2cde109ac30676ca
Raymond Brian D. Hernandez <35060465+raymondbrianhernandez@users.noreply.github.com>
2023-08-09 15:27:31 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!--
+██╗  ██╗██╗  ██╗██████╗  ██████╗ ███╗   ██╗ ██████╗ ███████╗    ██████╗     ██████╗ 
+██║ ██╔╝██║  ██║██╔══██╗██╔═══██╗████╗  ██║██╔═══██╗██╔════╝    ╚════██╗   ██╔═████╗
+█████╔╝ ███████║██████╔╝██║   ██║██╔██╗ ██║██║   ██║███████╗     █████╔╝   ██║██╔██║
+██╔═██╗ ██╔══██║██╔══██╗██║   ██║██║╚██╗██║██║   ██║╚════██║    ██╔═══╝    ████╔╝██║
+██║  ██╗██║  ██║██║  ██║╚██████╔╝██║ ╚████║╚██████╔╝███████║    ███████╗██╗╚██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝    ╚══════╝╚═╝ ╚═════╝ 
+June 7, 2023
+Raymond Brian D. Hernandez 
+Carla Regine R. Hernandez
+
+Revision History:
+     6/7/2023 - Project Started, Login page started (Raymond)
+     8/1/2023 - Template Khronos 1 to Khronos 2 (Raymond)
+     8/3/2023 - Registration page now has security questions for more secured login (Carla)
+-->
+
+<?php
+    // index.php will be simple, as it only displays 
+    // the revision histories then jump to login page.  
+    // No other codes will be posted here.
+
+    // Now let's go to login page...
+    // header("Location: https://khronos.pro/public/login.php"); 
+    header("Location: public/login.php");
+    exit();
+?>
